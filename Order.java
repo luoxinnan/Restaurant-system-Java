6c334ca9df11ece8b21eec7460e69fed538b02ec
Xinnan <luoxinnan1@gmail.com>
2023-04-09 17:59:20 +0200

    Mian classes

---
+++ Order.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+class Order{
+    private int number;
+    private String status;
+    private ArrayList<String> foods = new ArrayList<>();
+    private String type;
+    private boolean paid = false;
+}
