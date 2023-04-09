cabef94a97a55601b184f9fb9cc7b1208486b050
Xinnan <luoxinnan1@gmail.com>
2023-04-10 01:12:29 +0200

    Sende inn thread and deliver thread

--- OrderSystem.java
+++ OrderSystem.java
@@ -8,29 +8,31 @@ public class OrderSystem {
     public OrderSystem(){
     }
 
+    // add order to the list and append to order log
     public void addOrder(Order order){
         orders.add(order);
         addToOrderLog(order);
     }
 
+    // return the list of orders
     public ArrayList<Order> getOrders(){
         return orders;
     }
 
     public void addToOrderLog(Order order){
+        // write a line of information
         String foods = "";
         for(String foodName: order.getFoods().keySet()){
             foods = foods + foodName + " ";
         }
         String line = order.getNumber() + "," + order.getType() + "," + order.getTotalPrice() + "," + foods + "\n";
-        System.out.println(line);
 
+        // append to orderLog
         try {
             Files.write(Paths.get("orderLog.csv"), line.getBytes(), StandardOpenOption.APPEND);
         }catch (IOException e) {
             System.err.println(e);
         }
-
 
     }
 }
