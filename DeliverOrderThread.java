30b7eb4a3d27247862e6cc9e7638de3d9d1d005d
Xinnan <luoxinnan1@gmail.com>
2023-04-10 01:12:46 +0200

    send inn thread and deliver thread

--- DeliverOrderThread.java
+++ DeliverOrderThread.java
@@ -1,3 +1,18 @@
-public class DeliverOrderThread {
+public class DeliverOrderThread implements Runnable{
+    RestaurantMonitor monitor;
     
+    private DeliverOrderThread(RestaurantMonitor monitor){
+        this.monitor = monitor;
+    }
+
+    @Override
+    public void run(){
+        try{
+            monitor.packOrders();
+            System.out.println("Finished packing");
+        }catch(InterruptedException e){
+            System.err.println(e);
+            System.exit(1);
+        }
+    }
 }
--- SendInOrderThread.java
+++ SendInOrderThread.java
@@ -1,3 +1,21 @@
-public class SendInOrderThread {
+public class SendInOrderThread implements Runnable{
     
+    RestaurantMonitor monitor;
+    Order order;
+    
+    private SendInOrderThread(Order order, RestaurantMonitor monitor){
+        this.monitor = monitor;
+        this.order = order;
+    }
+
+    @Override
+    public void run(){
+        try{
+            monitor.workOnOrders(order);
+            System.out.println("Finished Sending");
+        }catch(InterruptedException e){
+            System.err.println(e);
+            System.exit(1);
+        }
+    }
 }
